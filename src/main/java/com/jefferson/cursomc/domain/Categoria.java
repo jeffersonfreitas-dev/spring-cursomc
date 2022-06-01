8a1c4567d78ea28cc7418b3c107748dcc1af31f9
jeffersonfreitas21 <jefferson.dev21@gmail.com>
2022-05-31 22:01:59 -0300

    Categoria e produto

---
+++ src/main/java/com/jefferson/cursomc/domain/Categoria.java
@@ -0,0 +1,31 @@
+package com.jefferson.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Categoria implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
+
+}
---
+++ src/main/java/com/jefferson/cursomc/domain/Produto.java
@@ -0,0 +1,39 @@
+package com.jefferson.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Produto implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Integer id;
+	private String nome;
+	private Double preco;
+	@ManyToMany
+	@JoinTable(
+			name="PRODUTO_CATEGORIA",
+			joinColumns = @JoinColumn(name = "id_produto"),
+			inverseJoinColumns = @JoinColumn(name = "id_categoria"))
+	private List<Categoria> categorias = new ArrayList<>();
+
+}
